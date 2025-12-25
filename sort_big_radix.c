31c5c6e997a47a6de38589731a8e886aa4481c7a
luozguo <anitadou9@gmail.com>
2025-12-25 15:56:52 +0100

    radix is done

--- sort_big_radix.c
+++ sort_big_radix.c
@@ -1,17 +1,28 @@
 #include "push_swap.h"
 
-void sort_big(t_stack *a, t_stack *b)
+// return the scope of bits through the largest value , ie. the largest index or the size after normalization
+static int get_max_bits(int max_value)
 {
-    int bit;
+    int bits;
+
+    bits = 0;
+    while ((max_value >> bits) != 0)
+        bits++;
+    return bits;
+}
+
+// outer loop : per bit;
+// inner loop 1: per element in the stack a
+// idea: push the 0s to b , after loop 1, push them back to a. this makes 0s, the smaller ones, stay on top.
+void sort_big_radix(t_stack *a, t_stack *b)
+{
     int max_bits;
+    int bit;
     int i;
     int size;
 
     size = a->size;
-    max_bits = 0;
-    while ((size - 1) >> max_bits)
-        max_bits++;
-
+    max_bits = get_max_bits(size - 1);
     bit = 0;
     while (bit < max_bits)
     {
