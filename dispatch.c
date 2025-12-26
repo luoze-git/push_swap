c6be7e135617c3f77575344c603b319227e63d2f
luozguo <anitadou9@gmail.com>
2025-12-26 16:29:10 +0100

    header file reorg

--- dispatch.c
+++ dispatch.c
@@ -6,7 +6,7 @@
 /*   By: luozguo <luozguo@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/26 14:59:30 by luozguo           #+#    #+#             */
-/*   Updated: 2025/12/26 15:06:55 by luozguo          ###   ########.fr       */
+/*   Updated: 2025/12/26 16:27:54 by luozguo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 void	dispatch_sort(t_stack *a, t_stack *b)
 {
-	if (is_sorted(a))
+	if (is_stack_sorted(a))
 		return ;
 	if (a->size <= 1)
 		return ;
@@ -22,18 +22,3 @@ void	dispatch_sort(t_stack *a, t_stack *b)
 	return ;
 }
 
-int	is_sorted(t_stack *a)
-{
-	t_node	*cur;
-
-	if (a->size < 2)
-		return (1);
-	cur = a->top;
-	while (cur->next)
-	{
-		if (cur->value > cur->next->value)
-			return (0);
-		cur = cur->next;
-	}
-	return (1);
-}
